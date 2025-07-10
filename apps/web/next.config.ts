61f2c3386cbc2b5f916f401e12dfe5ab8094390b
Erick Jesus <erick.jesus2060@gmail.com>
2025-07-10 20:48:51 -0300

    chore: update dockerfile

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  env: {
-    NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
-  },
 }
 
 export default nextConfig
