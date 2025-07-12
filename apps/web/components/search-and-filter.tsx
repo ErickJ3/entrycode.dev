707b58d4040dee34151890e0f0993067c61a7b3d
Erick Jesus <erick.jesus2060@gmail.com>
2025-07-11 21:19:06 -0300

    chore: update search filter

--- apps/web/components/search-and-filter.tsx
+++ apps/web/components/search-and-filter.tsx
@@ -6,7 +6,7 @@ import { Card } from '~/components/ui/card'
 
 export function SearchAndFilters() {
   return (
-    <Card className="border-white/10 p-4">
+    <Card className="p-0 border-none py-4">
       <div className="flex flex-col sm:flex-row gap-4">
         <div className="relative flex-1">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4" />
